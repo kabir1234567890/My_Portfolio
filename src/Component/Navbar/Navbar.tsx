20fd4f88ca58bbb622e857987b673fdd0b718594
Kabirahmed Hawawala <64394588+kabirahmedhawa@users.noreply.github.com>
2023-07-09 15:40:35 +0530

    making home page

--- src/Component/Navbar/Navbar.tsx
+++ src/Component/Navbar/Navbar.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { AppBar, Toolbar, Typography, Button, Avatar } from "@mui/material";
-import myImage from "../../assets/logo-01.png";
+
 import { Link } from "react-router-dom";
 const Navbar = () => {
+  const logoPath = process.env.REACT_APP_LOGO_PATH + "logo-01.png";
+  console.log(logoPath);
   return (
     <AppBar position="static">
       <Toolbar>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          <Avatar src={myImage} sx={{ width: 84, height: 84 }} />
+          <Avatar src={logoPath} sx={{ width: 84, height: 84 }} />
         </Typography>
         <Button component={Link} to="/" color="inherit" sx={{ mx: 2 }}>
           Home
@@ -16,7 +18,7 @@ const Navbar = () => {
           About
         </Button>
         <Button color="inherit" sx={{ mx: 2 }}>
-          portfolio
+          Blogs
         </Button>
         <Button color="inherit" sx={{ mx: 2 }}>
           Contact
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,45 @@
-import React from 'react'
-
+import { Box, Grid, Typography, Button } from "@mui/material";
+import React from "react";
+import Typewriter from "typewriter-effect";
+import { Link } from "react-router-dom";
+import "./style.css";
 const Home = () => {
+  const image = process.env.REACT_APP_LOGO_PATH + "homeBanner.png";
   return (
-    <div>Home</div>
-  )
-}
+    <Box>
+      <Grid container>
+        <Grid lg={1} md={1} xs={12}></Grid>
+        <Grid lg={6} md={6} xs={12} className={"homeTitle"}>
+          <Typography variant="h2">
+            <Typewriter
+              options={{
+                strings: [
+                  "Hello! ",
+                  "My name is Kabirahmed",
+                  "I am a Web Developer",
+                ],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </Typography>
+          <br />
+          <Typography variant="body1">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
+            nihil cum culpa placeat, molestiae rem nam itaque architecto
+            sapiente veritatis harum, dolorem quasi in inventore voluptas
+            laborum quibusdam. Eius, consequatur!
+          </Typography>
+          <Button variant="contained" component={Link} to="/about">
+            About me
+          </Button>
+        </Grid>
+        <Grid lg={5} md={5} xs={12}>
+          <img src={image} alt={"alt"} className="bannerImage" />
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
 
-export default Home
+export default Home;
